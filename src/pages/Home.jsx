91e89d21b1a3a9d8c6c4313d66402bde5a0c7d8e
Wawrzyniec Madej <wawrzyniec.madej@gmail.com>
2026-03-07 23:28:59 +0100

    b

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,8 +3,19 @@ import { albums } from "../data/albums.js"
 
 export default function Home() {
 
+    const folders2 = [
+        {
+            id: "10",
+            title: "LOFI collection 2",
+            image: "./albums/lofi-collection.jpg",
+            description: "A study of isolation through monochrome textures."
+        },
+    ];
+
     return (
             <div className="container mx-auto px-6">
+
+                {folders2.map(folder => <div>{folder.title}</div>)}
 
                 <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
 
